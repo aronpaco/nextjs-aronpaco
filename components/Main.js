8271668a4ef468a12d90a3684bf76d3e08ab90ba
aron <114922035+aronpaco@users.noreply.github.com>
2024-03-06 14:32:08 +0200

    feat: main.js

---
+++ components/Main.js
@@ -0,0 +1,8 @@
+export default function Main({ copyrightText }) {
+  return (
+    <main>
+      <p className="text-2xl dark:text-white text-center"></p>
+      <h1>Welcome!</h1>
+    </main>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -7,25 +7,15 @@ import Layout, { GradientBackground } from '../components/Layout';
 import ArrowIcon from '../components/ArrowIcon';
 import { getGlobalData } from '../utils/global-data';
 import SEO from '../components/SEO';
+import Main from '../components/Main';
 
 export default function Index({ posts, globalData }) {
   return (
     <Layout>
       <Header />
       <SEO title={globalData.name} description={globalData.blogTitle} />
-      <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
-          {globalData.blogTitle}
-        </h1>
-        <ul className="w-full">
-          {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            ></li>
-          ))}
-        </ul>
-      </main>
+      <Main />
+
       <Footer copyrightText={globalData.footerText} />
       <GradientBackground
         variant="large"
