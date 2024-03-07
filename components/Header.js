fd3e45450e7a0a23d1e5dc2b465f5ce7aa6dd5e1
aron <114922035+aronpaco@users.noreply.github.com>
2024-03-07 11:39:32 +0200

    bugfix: hydration error

--- components/Header.js
+++ components/Header.js
@@ -5,12 +5,12 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
-      <p className="text-2xl dark:text-white text-center">
+      <div className="text-2xl dark:text-white text-center">
         <div>
-          <Link href="/">Header</Link>
+          <Link href="/">Headers</Link>
           <ThemeSwitcher />
         </div>
-      </p>
+      </div>
     </header>
   );
 }
