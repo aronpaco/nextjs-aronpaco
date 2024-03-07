92cca5adc904acf4d8a5d7a3c88f914e0d7e14a2
aron <114922035+aronpaco@users.noreply.github.com>
2024-03-07 12:34:18 +0200

    feat: CssVarsProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import * as React from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import Sheet from '@mui/joy/Sheet';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <CssVarsProvider>
+        <Sheet variant="outlined">Hallo!</Sheet>
+      </CssVarsProvider>
       <span className="theme-bejamas" />
       <Component {...pageProps} />
     </>
